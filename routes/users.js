f912e690c628d3279ce5e8b75bba2e6bfcf7399a
safee <muhammad.safee@codedistrict.com>
2023-07-17 00:00:46 +0500

    bug fixes in auth and users

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,35 @@
 const { User, validate } = require("../models/user");
 
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
-    try {
-        const {error} = validate(req.body);
-        if (error)
-         return res.status(400).send({ message: error.details[0].message});
+  try {
+    console.log("body", req.body);
+    const { error } = validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
 
-        const user = await User.findOne({ email: req.body.email });
-        if (user)
-         return res.status(409).send({message: "User with givern email already exists!"})
+    const user = await User.findOne({ email: req.body.email });
+    if (user)
+      return res
+        .status(409)
+        .send({ message: "User with givern email already exists!" });
 
-        const salt = await bcrypt.genSalt(Number(process.env.SALT));
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
-        
-        await new User({...req.body, password: hashPassword}).save();
-    }
-    catch (error) {
-        res.status(500).send({message: "Internal Server Error"})
-    }
+    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = await new User({
+      ...req.body,
+      password: hashPassword,
+    }).save();
+    res
+      .status(200)
+      .send({ message: "User Signed up successfully", user: newUser });
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
